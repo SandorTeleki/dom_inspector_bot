51a0a13a9ccab1a9a08b83c7eb386f00dd12a5d5
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2022-06-26 12:47:34 +0200

    feat: basic bot skeleton

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# dom_inspector_bot
+# Discord Dom Inspector Bot "Abomination of Interspection"
+
+## A port of the Larzm's Mod Inspector: https://larzm42.github.io/dom5inspector/
+
+ToDo: rest of the README and add functionality to the bot.
+
+Plans:
+- Using embeds to display mod inspector info
+- Interacting with the API 
+- Expanding on slash commands
---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//Commands
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+//Events
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+//Set Bot activity
+client.on("ready", () => {
+	client.user.setActivity(' /items', { type: 'LISTENING' });
+});
+
+
+client.login(token);
