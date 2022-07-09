b113b63eb108c04a906e10df1c8f4d6a4f3b85cb
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2022-07-09 14:23:06 +0200

    hotfix: added console.log for debugging

--- commands/item.js
+++ commands/item.js
@@ -16,20 +16,18 @@ module.exports = {
             let fullBody = '';
             for await (const data of body) {
                 fullBody += data.toString();
-                console.log(fullBody);
+                console.log(`Console log 1: `+fullBody);
             }
             return JSON.parse(fullBody);
         }
-        
         const itemName = interaction.options.getString('item_name');
-		//const query = new URLSearchParams({ itemName });
-        console.log(ITEM_URL + encodeURIComponent(itemName)); //### passes input into URL and shows it in console.log
-
+		
 		const itemSearchResult = await request(ITEM_URL + encodeURIComponent(itemName));
+        console.log(`Console log 2: `+ITEM_URL + encodeURIComponent(itemName)); 
+        console.log(`Console log 3: `+itemSearchResult.body);
+        console.log(`Console log 4: `+itemSearchResult.body.toString())
 		const { list } = await getJSONResponse(itemSearchResult.body);
-        console.log(list)
-
-        // Tests for errors? Not working atm
+        console.log(`Console log 5: `+list)
 
 		if (!list.length) {
 			await interaction.reply(`No results found for **${itemName}**.`);
@@ -38,14 +36,14 @@ module.exports = {
 		const [answer] = list;
         
 		const itemEmbed = new MessageEmbed()
-            .setColor('#0099ff')
+            .setColor('#000000')
             .setTitle(answer.name)
-            .setURL('https://discord.js.org/')
-            .setAuthor({ name: 'Timotej & Toldi' })
-            .setDescription('Lot of hassle, but hey, it is working!')
+            .setURL('X')
+            .setAuthor({ name: 'Author' })
+            .setDescription('Lot of hassle, but hey, it was working!')
             .setImage(BASE_URL + answer.screenshot)
             .setTimestamp()
-            .setFooter({ text: 'A small step for Toldi, a giant leap for Discord Dom community' });
+            .setFooter({ text: 'A small step for X, a giant leap for X' });
         await interaction.reply({ embeds: [itemEmbed] });
 	},
 };
