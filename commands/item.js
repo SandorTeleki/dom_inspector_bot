1e7c6fdc816853278fbeafa34acc4c6050cc03c6
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2022-07-09 12:00:33 +0200

    feat: testing API requests

--- commands/item.js
+++ commands/item.js
@@ -1,7 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
-const { fetch } = require('node-fetch');
-const { ITEM_URL } = require('../utils/utils');
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { request } = require('undici');
+const { ITEM_URL, BASE_URL } = require('../utils/utils');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,18 +13,40 @@ module.exports = {
         .addStringOption(option => option.setName('item_name').setDescription('Enter the name of the item')),
 
 	async execute(interaction) {
-        const string = interaction.options.getString('item_name');
-        console.log(ITEM_URL + encodeURIComponent(string)); //### passes input into URL and shows it in console.log
-        // fetch(ITEM_URL + encodeURIComponent(string))
-        //     .then(res => res.JSON())
-        //     .then(JSON => console.log(JSON))
+        //console.log(ITEM_URL + encodeURIComponent(itemname)); //### passes input into URL and shows it in console.log
+        
+        async function getJSONResponse(body) {
+            let fullBody = '';
+        
+            for await (const data of body) {
+                fullBody += data.toString();
+            }
+        
+            return JSON.parse(fullBody);
+        }
+        
+        const itemName = interaction.options.getString('item_name');
+		//const query = new URLSearchParams({ itemName });
+
+		const itemSearchResult = await request(ITEM_URL + encodeURIComponent(itemName));
+		const { list } = await getJSONResponse(itemSearchResult.body);
+        console.log(list)
+
+        // Tests for errors? Not working atm
+
+		if (!list.length) {
+			await interaction.reply(`No results found for **${itemName}**.`);
+		}
+
+		const [answer] = list;
+        
 		const itemEmbed = new MessageEmbed()
             .setColor('#0099ff')
-            .setTitle('Frost Brand')
+            .setTitle(answer.name)
             .setURL('https://discord.js.org/')
             .setAuthor({ name: 'Timotej & Toldi' })
             .setDescription('Lot of hassle, but hey, it is working!')
-            .setImage('https://dom5api.illwiki.com/items/14/screenshot')
+            .setImage(BASE_URL + answer.screenshot)
             .setTimestamp()
             .setFooter({ text: 'A small step for Toldi, a giant leap for Discord Dom community' });
         await interaction.reply({ embeds: [itemEmbed] });
--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,4 @@
 module.exports = {
+    BASE_URL: 'https://dom5api.illwiki.com',
     ITEM_URL: 'https://dom5api.illwiki.com/items?match=fuzzy&name=',
 };
