0167334c4daba62db39a6d074aafaf4653862bfe
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2022-07-18 12:54:03 +0200

    feat: updated help with new functionality

--- commands/help.js
+++ commands/help.js
@@ -16,6 +16,7 @@ module.exports = {
                 { name : '```/merc {merc_name}```', value: 'Use this command to search for mercenaries. Replace {merc_name} with the name of the mercenary group (not the boss name, although we might add that later). Minor typos in merc name are fine.'},
                 { name : '```/site {site_name}```', value: 'Use this command to search for sites. Replace {site_name} with the name of the site. Minor typos in site name are fine.'},
                 { name : '```/event {event_name}```', value: 'work in progress'},
+                { name : '```?{command_name}```', value: 'All slash commands will work if you type them normally with the "?" prefix before them. Like "?item frost brand"'},
                 { name : 'Source/background', value: "Based on Larzm42's Dominions 5 Inspector: [Dom5Inspector](https://larzm42.github.io/dom5inspector/)"},
             )
             .setFooter({ text: "A bot by Timotej and Toldi.\nOriginally created for the Immersion server!" });
--- index.js
+++ index.js
@@ -61,7 +61,7 @@ for (const file of eventFiles) {
 
 //Set Bot activity
 client.on("ready", () => {
-	client.user.setActivity(' slash commands', { type: 'LISTENING' });
+	client.user.setActivity(' ?name or slash commands', { type: 'LISTENING' });
 });
 
 //Bot Login
@@ -173,6 +173,7 @@ client.on("messageCreate", async (message) => {
                 { name : '```/merc {merc_name}```', value: 'Use this command to search for mercenaries. Replace {merc_name} with the name of the mercenary group (not the boss name, although we might add that later). Minor typos in merc name are fine.'},
                 { name : '```/site {site_name}```', value: 'Use this command to search for sites. Replace {site_name} with the name of the site. Minor typos in site name are fine.'},
                 { name : '```/event {event_name}```', value: 'work in progress'},
+				{ name : '```? {command_name}```', value: 'All slash commands will work if you type them normally with the "?" prefix before them. Like "?item frost brand"'},
                 { name : 'Source/background', value: "Based on Larzm42's Dominions 5 Inspector: [Dom5Inspector](https://larzm42.github.io/dom5inspector/)"},
             )
             .setFooter({ text: "A bot by Timotej and Toldi.\nOriginally created for the Immersion server!" });
