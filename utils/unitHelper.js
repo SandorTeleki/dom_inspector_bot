5493e47e798cea96601bb881ddce92eb65d9919e
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2022-08-08 13:24:25 +0200

    feat: added random paths to mages

--- utils/unitHelper.js
+++ utils/unitHelper.js
@@ -8,22 +8,36 @@ async function getUnit( unitName ){
     if (unitName in unitAliases){ unitName = unitAliases[unitName] };
     var unit;
     var similarMatchesString;
+    var footerStrings = '';
     if  (/^\d+$/.test(unitName)){
         const { body } = await request(BASE_URL + UNIT_URL + '/' + encodeURIComponent(unitName));
         unit  = await body.json();
     }
-
     else {
         const { body } = await request(BASE_URL + UNIT_URL + FUZZY_MATCH_URL + encodeURIComponent(unitName));
         var { units } = await body.json();
         unit = units[0];
-        similarMatchesString = similarMatches(units);
+        similarMatchesString = similarMatches(units);  
     }; 
+
     const unitEmbed = new MessageEmbed()
         //.setTitle(unit.name)
         //.setDescription('Mentor notes will go here.')
-        .setImage(BASE_URL + unit.screenshot);
-    if ( similarMatchesString ) {unitEmbed.setFooter({text: similarMatchesString})};
+        .setImage(BASE_URL + unit.screenshot)
+        if (unit.randompaths !== undefined){
+            for ( const randompath of unit.randompaths ) {
+                footerStrings += ( randompath.chance + '%' + ' of' + ' +' + randompath.levels + ' ' + randompath.paths+ ' ' +  "\n");
+            }
+        }
+  
+        if ( similarMatchesString ) {
+            unitEmbed.setFooter({text: footerStrings + similarMatchesString})
+        }
+        else {
+            unitEmbed.setFooter({ text: footerStrings });
+        };
+        
+        
     return unitEmbed;
 }
 
