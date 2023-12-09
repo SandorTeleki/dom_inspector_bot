f6a9551e50bb262fa7dfeb407ec4b203e4321ab6
Sandor Teleki <sandorteleki1@gmail.com>
2023-12-09 10:10:19 +0100

    feat: refactored updateNote function

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const { getUnit } = require('./utils/unitHelper');
 const { getHelpEmbed } = require('./utils/helpEmbed');
 const { WRONG_BOT_URL, ALL_BOOLI_URL, BASE_URL } = require('./utils/utils');
 const { mentorWhitelist, channelWhiteList } = require('./utils/whitelist');
+const { updateNote } = require('./utils/updateNote');
 
 
 const client = new Client({ 
@@ -285,29 +286,7 @@ client.on("messageCreate", async (message) => {
 					}
 			
 					//Since a match was found, we UPDATE the note
-					function updateNote() {
-						sql = `UPDATE mentor_notes SET note = ? WHERE class = ? AND class_id = ? AND guild_id = ?`
-						db.run(sql,[noteWritten,commandUsed,idUsed,serverId],(err)=> {
-							if(err) return console.error(err.message);
-						});
-
-						//Adding command log usage
-						sql = `INSERT INTO logs(server_name,server_id,channel_name,channel_id,user_name,user_id,chat_command,unix_timestamp) VALUES (?,?,?,?,?,?,?,?)`
-						db.run(sql,[server,serverId,channelName,channelId,user,userId,text,unixTimestamp],(err) => {
-							if(err) return console.error(err.message);
-						});
-
-						//Adding mentor note log
-						sql = `INSERT INTO mentor_logs(class,class_id,name,note,guild_name,guild_id,written_time,written_by_user) VALUES (?,?,?,?,?,?,?,?)`
-						db.run(sql,[commandUsed,idUsed,commandName,noteWritten,server,serverId,unixTimestamp,user],(err) => {
-							if(err) return console.error(err.message);
-						});
-						
-						console.log("Note was found, updating note...")
-						message.reply("Note was updated!")
-
-					}
-					updateNote();
+					updateNote(message, noteWritten, commandUsed, idUsed, serverId, server, channelName, channelId, user, userId, text, unixTimestamp);
 				});
 			}
 			checkNoteMatch();
