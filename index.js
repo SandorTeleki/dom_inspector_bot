2997229a6401ba57a8318bd0f295bf2148393645
Sandor Teleki <sandorteleki1@gmail.com>
2023-12-13 09:30:31 +0100

    fix: added comment to note syntax explanation

--- index.js
+++ index.js
@@ -205,10 +205,12 @@ client.on("messageCreate", async (message) => {
 			message.reply('This channel is not whitelisted to use the `?note` command.');
 			return;
 		};
+		//Explanation for note syntax and rules
 		if (message.content === "?note"){
 			message.reply("The syntax is: `?note {class} {id} {text}`\n`{class}` is the name of the command (item, merc, unit etc.) \n`{id}` is the id of the item, merc, unit etc. \n`{text}` is the text of your mentor note (cannot be blank). \n Note: the current note min-length is "+`\`${noteLengthLimitMin}\``+" characters and the max-length is "+`\`${noteLengthLimitMax}\``+" characters.");
 			return;
 		}
+		
 		//Split the note into matching groups using regex to make error checking and logging easier
 		const regEx = /^(item|spell|unit|site|merc)\s(\d+)\s(.*)/i;
 		const note = message.content.slice(6);
