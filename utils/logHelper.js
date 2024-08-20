1f650b17487a34309c1598c6fad8199f4048f376
Sandor Teleki <sandorteleki1@gmail.com>
2024-08-20 15:29:27 +0200

    fix: removed 'var' from logHelper.js

--- utils/logHelper.js
+++ utils/logHelper.js
@@ -2,6 +2,7 @@ const { sqlInsertLog } = require('./sqlHelper');
 
 // Creates log for SQL
 function createLog(data){
+	let text
 	//Read more: https://old.discordjs.dev/#/docs/discord.js/main/search?query=message
 	// and: https://old.discordjs.dev/#/docs/discord.js/main/class/Message
 	const server = data.guild.name;
@@ -10,7 +11,7 @@ function createLog(data){
 	const channelId = (data.type === 0 ? data.channelId : data.channel.id );
 	const user = (data.type === 0 ? data.author.tag : data.user.tag );
 	const userId = (data.type === 0 ? data.author.id : data.user.id );
-	if (data.type === 0){ var text = data.content } else if (data.type === 2){ var text = data.toString()} else {var text = data.customId};
+	if (data.type === 0){ text = data.content } else if (data.type === 2){ text = data.toString() } else { text = data.customId };
 	const unixTimestamp = data.createdTimestamp;
 
 	sqlInsertLog(server,serverId,channelName,channelId,user,userId,text,unixTimestamp);
