73d3284b2ff0ea6e1fc130f45769d0cec307bf51
Sandor Teleki <sandorteleki1@gmail.com>
2024-08-30 10:15:14 +0200

    feat: added tests for button creator

---
+++ test/buttonCreator.test.js
@@ -0,0 +1,19 @@
+//no max, so test it with nothing, 1, 5, 100
+import { describe, expect, it, vi } from 'vitest';
+import { buttonCreator } from '../utils/buttonCreator';
+
+describe('Test button creator functionality', () => {
+    it('it should return an empty array if there are no similar matches', () => {
+        const similarMatchesList = [];
+        const prefix = 'test';
+        const buttons = buttonCreator(similarMatchesList, prefix);
+        expect(buttons.length).toBe(0);
+    });
+
+    it('it should return 5 buttons if there are 5 similar matches', () => {
+        const similarMatchesList = [{id: 1, name: '1'}, {id: 2, name: '2'}, {id: 3, name: '3'}, {id: 4, name: '4'}, {id: 5, name: '5'}];
+        const prefix = 'test';
+        const buttons = buttonCreator(similarMatchesList, prefix);
+        expect(buttons.length).toBe(5);
+    })
+})
