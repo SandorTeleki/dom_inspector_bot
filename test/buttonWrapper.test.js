cb1983d7dd526f18bfe696bf83a3d984ba0b72a8
Sandor Teleki <sandorteleki1@gmail.com>
2024-08-30 10:10:39 +0200

    fix: changed test to it for consistency

--- test/buttonWrapper.test.js
+++ test/buttonWrapper.test.js
@@ -1,15 +1,15 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { buttonWrapper } from '../utils/buttonWrapper';
 
 describe('Test button wrapper functionality', () => {
-    test('returns a partial row of arrays if it receives 4 buttons', () => {
+    it('returns a partial row of arrays if it receives 4 buttons', () => {
         const buttons = Array.from({ length: 4 }, (_, i) => ({ id: `button${i + 1}` }));
         const components = buttonWrapper(buttons);
         expect(components.length).toBe(1);
         expect(components[0].components.length).toBe(4);
     })
 
-    test('returns one full and a partial row of arrays if it receives 9 buttons', () => {
+    it('returns one full and a partial row of arrays if it receives 9 buttons', () => {
         const buttons = Array.from({ length: 9 }, (_, i) => ({ id: `button${i + 1}` }));
         const components = buttonWrapper(buttons);
         expect(components.length).toBe(2);
@@ -17,7 +17,7 @@ describe('Test button wrapper functionality', () => {
         expect(components[1].components.length).toBe(4);
     })
 
-    test('returns five full rows of arrays if it receives 25 buttons', () => {
+    it('returns five full rows of arrays if it receives 25 buttons', () => {
         const buttons = Array.from({ length: 25 }, (_, i) => ({ id: `button${i + 1}` }));
         const components = buttonWrapper(buttons);
         expect(components.length).toBe(5);
@@ -28,7 +28,7 @@ describe('Test button wrapper functionality', () => {
         expect(components[4].components.length).toBe(5);
     })
 
-    test('returns five full rows of arrays even if it receives more than the maximum amount of buttons', () => {
+    it('returns five full rows of arrays even if it receives more than the maximum amount of buttons', () => {
         const buttons = Array.from({ length: 30 }, (_, i) => ({ id: `button${i + 1}` }));
         const components = buttonWrapper(buttons);
         expect(components.length).toBe(5);
--- test/randomNumber.test.js
+++ test/randomNumber.test.js
@@ -1,24 +1,24 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { getRandomNumber } from '../utils/randomNumber'
 import { minRandomNumber, maxRandomNumber } from '../utils/utils';
 
 describe('Test random number functionality', () => {
-    test('returns a random integer between 0 and the provided number', () => {
+    it('returns a random integer between 0 and the provided number', () => {
         const input = 100;
         expect(getRandomNumber(100)).toBeLessThanOrEqual(input);
     })
 
-    test('returns an error if the provided number is below the minimum number', () => {
+    it('returns an error if the provided number is below the minimum number', () => {
         const input = 0;
         expect(getRandomNumber(input)).toEqual(`Syntax error. Please only input integers between \`${minRandomNumber}\` and \`${maxRandomNumber}\`.\nYour number was \`${Math.abs(minRandomNumber - input)}\` below the min.`)
     })
 
-    test('returns an error if the provided number is above the maximum number', () => {
+    it('returns an error if the provided number is above the maximum number', () => {
         const input = 1000;
         expect(getRandomNumber(input)).toEqual(`Syntax error. Please only input integers between \`${minRandomNumber}\` and \`${maxRandomNumber}\`.\nYour number was \`${input - maxRandomNumber}\` above the max.`)
     })
 
-    test('returns an error if NaN (not a number) was provided', () => {
+    it('returns an error if NaN (not a number) was provided', () => {
         const input = 'blah';
         expect(getRandomNumber(input)).toEqual(`Syntax error. Please only input integers between \`${minRandomNumber}\` and \`${maxRandomNumber}\`.`)
     })
